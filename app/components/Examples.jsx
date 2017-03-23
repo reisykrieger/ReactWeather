9579e99ac7949aab7fc91cd787d99f9f3314468c
Reisy Krieger <reisykrieger@gmail.com>
2017-03-23 11:37:11 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,19 @@
 var React = require ('react');
 
-// var Examples = React.createClass({
-//   render:function(){
-//     return(
-//       <h3>Examples component</h3>
-//     )
-//   };
-// });
+var Examples = React.createClass({
+  render:function(){
+    return(
+      <h3>Examples component</h3>
+    )
+  };
+});
 
 var Examples = (props) =>{
   return(
+    <div>
     <h3>Examples component!</h3>
+    <P>Welcome to examples page</P>
+    </div>
   )
 };
 module.exports = Examples;
