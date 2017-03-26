7bf15cebaee554ee0654bfc129182b3162188970
Reisy Krieger <reisykrieger@gmail.com>
2017-03-26 11:11:15 +0300

    Custom css added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var React = require ('react');
 var About = (props) =>{
   return(
     <div>
-    <h1 className="text-center">About</h1>
+    <h1 className="text-center page-title">About</h1>
     <p>Welcome to the about page this is very boring and I can't be bothered</p>
     <p>
       Here are some of the tools I have used:
